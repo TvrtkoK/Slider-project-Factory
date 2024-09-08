0b8a5940f9b13034c35e4b538e2f286b91b3f2bc
Tvrle <tvrtko.krpan27@gmail.com>
2024-09-08 18:50:41 +0200

    Improved code.

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
-// script.js
-$(document).ready(function() {
-    var slideWidth = $('.arrow').width();
-    var sliding = false;
-
-    function moveRight() {
-        if (sliding) return;
-        sliding = true;
-        $('.row').each(function() {
-            $(this).animate({
-                transform: 'translateX(' + slideWidth + 'px)'
-            }, 500, function() {
-                $(this).prepend($(this).children().last());
-                $(this).css('transform', 'translateX(0)');
-                sliding = false;
-            });
+$(document).ready(() => {
+    let sliding = false;
+    const moveRight = () => {
+      if (sliding) return;
+      sliding = true;
+      $('.row').each(function () {
+        let $row = $('.row');
+        let $lastImage = $row.find('img').last();
+        $lastImage.addClass('animate-slide-right');
+        setTimeout(() => {
+          $(this).prepend($(this).children().last());
+          $lastImage.removeClass('animate-slide-right');
+          sliding = false;
         });
-    }
-
-    function moveLeft() {
-        if (sliding) return;
-        sliding = true;
-        $('.row').each(function() {
-            $(this).animate({
-                transform: 'translateX(-' + slideWidth + 'px)'
-            }, 500, function() {
-                $(this).append($(this).children().first());
-                $(this).css('transform', 'translateX(0)');
-                sliding = false;
-            });
-        });
-    }
-
-    $('.arrow.left').click(function() {
-        moveLeft();
+      }, 500);
+    };
+    const moveLeft = () => {
+      if (sliding) return;
+      sliding = true;
+      $('.row').each(function () {
+        $(this).addClass('animate-slide-left');
+        setTimeout(() => {
+          $(this).append($(this).children().first());
+          $(this).removeClass('animate-slide-left');
+          sliding = false;
+        }, 500);
+      });
+    };
+    $('.arrow.left-arrow').click(() => {
+      moveLeft();
     });
-
-    $('.arrow.right').click(function() {
-        moveRight();
+    $('.arrow.right-arrow').click(() => {
+      moveRight();
     });
-});
+  });
